fd606ba0845968838c56a89c8e7be4e5419c1218
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-13 14:05:10 -0300

    feat: add database insert

--- src/modules/User/service.ts
+++ src/modules/User/service.ts
@@ -1,13 +1,21 @@
-import { validateRequest } from "utils/zod.js";
-import { userValidateSchemaRequest } from "./entity.js";
+import { validateRequest } from "utils/zod/zod.js";
+import { userValidateSchemaRequest } from "./schema/entity.js";
+import { AppDataSource } from "database/data-source.js";
+import { User } from "database/entity/User.js";
 
 export class UserService {
   async create(bodyData: any) {
     console.log("API BODY", bodyData);
     if (!bodyData) throw new Error("Não existe conteúdo na requisição");
 
-    const validate = await validateRequest(bodyData, userValidateSchemaRequest);
+    // Validação dos dados
+    await validateRequest(bodyData, userValidateSchemaRequest);
 
-    return validate;
+    const messageRepository = AppDataSource.getRepository(User);
+
+    // Remover checkEmail porque não vai ser inserido no banco
+    delete bodyData?.checkEmail;
+
+    await messageRepository.save(bodyData);
   }
 }
