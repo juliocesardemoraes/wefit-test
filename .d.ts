65497dafea033850d0d30344651a03f5e68ce44d
juliocesardemoraes <jcmcf2012@gmail.com>
2024-11-13 18:46:21 -0300

    docs: force false strict

--- .d.ts
+++ .d.ts
@@ -1,11 +1,1 @@
-declare module "faker-br" {
-  export const faker: {
-    br: {
-      cpf: () => string;
-      cnpj: () => string;
-      rg: () => string;
-      cep: () => string;
-      // Add other methods available in `faker-br` here as needed
-    };
-  };
-}
+declare module "faker-br" {}
